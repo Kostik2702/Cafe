a84a76749f4604ebe100d21303b616fc482ac36b
Kostik2702 <hamster177@yandex.ru>
2016-01-21 20:42:00 +0200

    +Admin controller class fixed

--- src/main/java/com/kos/cafe/control/AdminController.java
+++ src/main/java/com/kos/cafe/control/AdminController.java
@@ -1,6 +1,8 @@
 package com.kos.cafe.control;
 
 import com.kos.cafe.domain.Comment;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,7 +17,20 @@ public class AdminController {
     public ModelAndView start(ModelAndView model){
 
         ModelAndView modelAndView = new ModelAndView("admin");
+        modelAndView.addObject("user", getPrincipal());
 
         return modelAndView;
+    }
+
+    private String getPrincipal(){
+        String userName = null;
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        if (principal instanceof UserDetails) {
+            userName = ((UserDetails)principal).getUsername();
+        } else {
+            userName = principal.toString();
+        }
+        return userName;
     }
 }
